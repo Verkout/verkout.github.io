8475a49bf8972254973cc9ae47fd4b0a15f8e601
Brian Dainton <brian.dainton@gmail.com>
2020-06-25 13:14:38 -0500

    Add axios. Make an api call to get workout data.

--- src/components/WorkoutComponent.js
+++ src/components/WorkoutComponent.js
@@ -1,14 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Switch, Route, Link, useParams } from "react-router-dom";
 import PageNotFound from "./PageNotFound";
+import axios from "axios";
 import s from "../styles/workoutComponent.style";
+import regeneratorRuntime from "regenerator-runtime";
 
 export default function WorkoutComponent(props) {
+  const workoutId = props.match.params.workoutId;
+  const [workout, setWorkout] = useState();
+  const [isFetchingWorkout, setIsFetchingWorkout] = useState(false);
+
   useEffect(() => {
     console.log("in useEffect!");
-  });
 
-  var workoutId = props.match.params.workoutId;
+    const fetchWorkout = async () => {
+      var url =
+        "https://w3ikkohwal.execute-api.us-east-1.amazonaws.com/prod/workouts/" +
+        workoutId;
+
+      const result = await axios(url);
+      console.log("got result:");
+      console.log(result);
+      if (result && result.data) {
+        setWorkout(result.data.data);
+      }
+    };
+
+    if (workoutId && !workout && !isFetchingWorkout) {
+      setIsFetchingWorkout(true);
+      fetchWorkout();
+    }
+  }, []);
 
   if (!workoutId) {
     return <PageNotFound />;
@@ -17,6 +39,7 @@ export default function WorkoutComponent(props) {
   return (
     <div style={s.root}>
       <p>workoutId = {workoutId}</p>
+      <p>{workout && workout.name}</p>
     </div>
   );
 }
